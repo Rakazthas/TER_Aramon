232c5e5c750e54c8282f091ed0932867d42f1d39
Rakazthas <guillaume.lsn@gmail.com>
2021-03-11 15:56:09 +0100

    first re-ordering

---
+++ Assets/Scripts/Descendre.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Descendre : MonoBehaviour
+{
+
+    public Transform playerBody;
+    public Transform Target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown(){
+        playerBody.transform.position = Target.transform.position;
+        
+    }
+}
